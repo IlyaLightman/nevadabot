a87a5b464c412ddb6df8bd99b0d1cf98f6215292
Ilya Zaytsev <ilya@zaytsev.xyz>
2021-07-09 13:39:14 +0300

    promise data updater

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const { Telegraf } = require('telegraf')
+const redis  = require('redis')
+const redisClient = redis.createClient()
 
 const { dataUpdater } = require('./dataController')
 
 const start = async () => {
-	await dataUpdater()
+	await dataUpdater(redisClient)
 }
 start().then(() => {
 	console.log('ok')
--- dataController.js
+++ dataController.js
@@ -4,53 +4,61 @@ const fs = require('fs')
 require('dotenv').config()
 
 const dataUpdater = async redisClient => {
-	await dataDownloading(1)
+	try {
+		await dataDownloading(1)
 
-	return
-	// Loading to the cache
-	const lr_db2 = new sqlite3.Database('./data/side/lr_base.sq3')
-	const vip_db2 = new sqlite3.Database('./data/side/vip_core.sq3')
+		// Loading to the cache
+		const lr_db2 = new sqlite3.Database('./databases/side/lr_base.sq3')
+		const vip_db2 = new sqlite3.Database('./databases/side/vip_core.sq3')
 
-	const selectLvlBase = 'SELECT * FROM lvl_base'
-	const selectVipBase = 'SELECT * FROM vip_users'
+		const selectLvlBase = 'SELECT * FROM lvl_base'
+		const selectVipBase = 'SELECT * FROM vip_users'
 
-	await lr_db2.all(selectLvlBase, [], (err, rows) => {
-		if (err) return console.log('side lr', err)
-		redisClient.set('side/lr', JSON.stringify(rows), () => {})
-	})
-	await vip_db2.all(selectVipBase, [], (err, rows) => {
-		if (err) return console.log('side vip', err)
-		redisClient.set('side/vip', JSON.stringify(rows), () => {})
-	})
-
-	await lr_db2.close()
-	await vip_db2.close()
-}
-
-const dataDownloading = async srvNumber => {
-	// 0 - Nevada, 1 - Side, 2 - Future
-
-	// FTP Download
-	const client = new ftp()
-	client.on('ready', () => {
-		client.get('212.22.93.106_27045/addons/sourcemod/data/sqlite/lr_base.sq3',
-			(err, stream) => {
-				if (err) return console.log('fucking', err)
-
-				stream.pipe(fs.createWriteStream('databases/side/lr_base.sq3'))
-				stream.once('close', () => client.end())
+		await lr_db2.all(selectLvlBase, [], (err, rows) => {
+			if (err) return console.log('side lr', err)
+			redisClient.set('side/lr', JSON.stringify(rows), () => {})
+		})
+		await vip_db2.all(selectVipBase, [], (err, rows) => {
+			if (err) return console.log('side vip', err)
+			redisClient.set('side/vip', JSON.stringify(rows), () => {})
 		})
 
-		client.get('212.22.93.106_27045/addons/sourcemod/data/sqlite/vip_core.sq3',
-			(err, stream) => {
-				if (err) return console.log('fucking', err)
+		await lr_db2.close()
+		await vip_db2.close()
+	} catch (err) {
+		console.log(err)
+	}
+}
 
-				stream.pipe(fs.createWriteStream('databases/side/vip_core.sq3'))
-				stream.once('close', () => client.end())
-			})
+const dataDownloading = srvNumber => {
+	// 0 - Nevada, 1 - Side, 2 - Future
+
+	return new Promise((resolve, reject) => {
+		// FTP Download
+		const client = new ftp()
+		client.on('ready', () => {
+			client.get('212.22.93.106_27045/addons/sourcemod/data/sqlite/lr_base.sq3',
+				(err, stream) => {
+					if (err) return console.log('fucking', err)
+
+					stream.pipe(fs.createWriteStream('databases/side/lr_base.sq3'))
+					stream.once('close', () => client.end())
+				})
+
+			client.get('212.22.93.106_27045/addons/sourcemod/data/sqlite/vip_core.sq3',
+				(err, stream) => {
+					if (err) return console.log('fucking', err)
+
+					stream.pipe(fs.createWriteStream('databases/side/vip_core.sq3'))
+					stream.once('close', () => {
+						client.end()
+						resolve('Data downloaded')
+					})
+				})
+		})
+
+		client.connect(connectionData[srvNumber])
 	})
-
-	client.connect(connectionData[srvNumber])
 }
 
 const connectionData = [
