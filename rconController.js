6ebbfb4974d20c3429408ab83d5a9f1b6fb4451d
Ilya Zaytsev <ilya@zaytsev.xyz>
2021-07-11 22:26:08 +0300

    rcon connection

--- rconController.js
+++ rconController.js
@@ -8,19 +8,20 @@ const options = {
 }
 
 const connSide = new Rcon(connectionData[1].host, 27045, process.env.SIDE_RCON, options)
-connSide.on('auth', () => console.log('Rcon Side Authed')).on('response', console.log)
+connSide
+	.on('auth', () => console.log('Rcon Side Authed'))
+	// .on('response', console.log)
 
 const connection = async () => {
 	try {
 		await connSide.connect()
-		// await connSide.send(`rcon_password ${process.env.SIDE_RCON}`)
+
+		// setTimeout(() => connSide.send('status'), 6000)
 	} catch (err) {
 		console.log('Auth Rcon Failed', err)
 	}
 }
 connection().then()
-
-console.log('fuck')
 
 const rconSideCommand = command => connSide.send(command)
 
